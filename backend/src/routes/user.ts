59c61f7c19cd91c902d1c3c8ed962b6049392feb
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-27 14:14:42 +0530

    Add signin endpoint

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from "express";
 import signupBodySchema from "../types/signup";
 import { z } from "zod";
+import Jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { User } from "../database/db";
+import signinBodySchema from "../types/signin";
+import { JWT_SECRET } from "../config";
 const userRouter: any = Router();
 
 type signupBodyType = z.infer<typeof signupBodySchema>;
@@ -21,8 +24,8 @@ userRouter.post("/signup", async (req: Request, res: Response) => {
   });
 
   if (existingUser) {
-    return res.status(411).json({
-      message: "User already exist",
+    return res.status(403).json({
+      message: "User already exist with this username",
     });
   }
 
@@ -35,6 +38,52 @@ userRouter.post("/signup", async (req: Request, res: Response) => {
 
   res.json({
     message: "Signed up",
+  });
+});
+
+type signinBodyType = z.infer<typeof signinBodySchema>;
+
+userRouter.post("/signin", async (req: Request, res: Response) => {
+  const signinBody: signinBodyType = req.body;
+  const { success } = signinBodySchema.safeParse(req.body);
+
+  if (!success) {
+    return res.status(411).json({
+      messge: "Error in inputs",
+    });
+  }
+  const existingUser = await User.findOne({
+    username: signinBody.username,
+  });
+
+  if (!existingUser) {
+    return res.status(404).json({
+      message: "User does not exist",
+    });
+  }
+
+  const passwordMatch = await bcrypt.compare(
+    signinBody.password,
+    existingUser.password
+  );
+
+  if (passwordMatch) {
+    const token = Jwt.sign(
+      {
+        userId: existingUser._id,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      message: "You are signed in",
+      token: token,
+    });
+    return;
+  }
+
+  res.status(411).json({
+    message: "Wrong credentials",
   });
 });
 
